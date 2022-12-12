2c4d0aa09c6854e22f9100870b0ba84cbedcd60c
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2022-12-12 09:42:42 +0000

    solved

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,5 +1,22 @@
-<include stdio.h>
+#include <stdio.h>
+
+void scalene(int side1,int side2,int side3);
+
+int main()
 {
-  int main()
+  int side1,side2,side3;
+  printf("Enter the side :");
+  scanf("%d%d%d",&side1,&side2,&side3);
   
+
+  scalene(side1,side2,side3);
+}
+
+void scalene(int side1,int side2,int side3){
+  if (side1==side2 || side2==side3 || side3==side1){
+    printf("The given triangle is not scalene :(");
+    }
+  else{
+    printf("The given triangle is  scalen :)");
+  }
 }
