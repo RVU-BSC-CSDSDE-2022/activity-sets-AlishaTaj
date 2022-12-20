9015466f7b98933bb7665505df0cf64a1e0a8700
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2022-12-20 08:58:26 +0000

    solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,34 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 int input();
-void add(int a, int b, int *sum);
-int change_number (int *a);
-int output (int a, int b, int sum);
-
-int input;
+void add (int a, int b, int *sum);
+void add(int a , int b, int sum);
+int input()
 {
- int n;
-  printf("enter the number")
-  scanf ("%d",&n);
-  return n;
-  }
-int change_number (int*a){
-  *a=6;
-  }
-
-void add(int a, int b, int *sum)
+	int n;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	return n;
+}
+void add(int a , int b , int *sum)
 {
-  *sum=a+b;
-  }
-int output(int a, int b, int sum)
+*sum=a+b;
+}
+void output(int a, int b ,int sum)
 {
-  printf("the sum of %d and %d number %d\n",a,b,sum);
-  }
+printf("sum of %d and %d is%d",a,b,sum);
+}
 int main()
 {
-  int a,b,sum;
-  a=input();
-  b=input();
-  change_number(&a);
-  add(a,b,&sum);
-  printf("the sum of the number %d\n",sum);
-  }
+int a,b,sum;
+a=input();
+b=input();
+add(a,b,&sum);
+}
