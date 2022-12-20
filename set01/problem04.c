c5ac6babee9b4f3e1a42d8513bcd4bdf55b01283
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2022-12-20 09:01:20 +0000

    solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -15,7 +15,7 @@ void add(int a , int b , int *sum)
 }
 void output(int a, int b ,int sum)
 {
-printf("sum of %d and %d is%d",a,b,sum\n);
+printf("sum of %d and %d is%d\n",a,b,sum);
 }
 int main()
 {
