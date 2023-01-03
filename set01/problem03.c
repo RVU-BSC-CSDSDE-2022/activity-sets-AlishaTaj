6fe1d762d5dbc1ba673a1355ba40338b58eb713e
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-01-03 09:52:49 +0000

    solved

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,28 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
 int add(int a, int b);
-void main( int a,  int b, int sum);
-int input()
-{
-	int n;
-	printf("enter the number:);
-	scanf("%d",&n);
-	return n;
+void output(int a, int b, int sum);
+int main(){
+   int a , b , sum;
+   printf("enter the two numbers");
+   a=input();
+   b=input();
+   sum= add(a,b)
+   output(a , b , sum);
+} int input
+    int a;
+  scanf("%d", &a);
+  return a ;
 }
-int add( int a , int b)
-{
-	int sum;
-	sum=a+b;
+int add(int a, int b){
+  int sum;
+  sum = a + b;
+  return sum;
 }
-void main( int a, int b , int sum)
-{
-	printf("the addition of number is %d\n",sum);
-}
-int main()
-{
-	int a,b,sum;
-	a=input();
-	b=input();
-	add(a,b);
+ void output(int a, int b, int sum){
+printf("The sum of %d and %d is: %d\n",a,b,sum);
 }
 
--- set01/problem04.c
+++ set01/problem04.c
@@ -1,26 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
-void add (int a, int b, int *sum);
-void add(int a , int b, int sum);
-int input()
-{
-	int n;
-	printf("enter the number\n");
-	scanf("%d",&n);
-	return n;
+void add(int a, int b,int *sum);
+void output(int a,int b, int sum);
+
+int input(){
+  int n;
+ printf("Enter a number:\n");
+  scanf("%d",&n);
+  return n;
 }
-void add(int a , int b , int *sum)
-{
-*sum=a+b;
+
+void add(int a, int b, int *sum){
+ *sum= a+b;
 }
-void output(int a, int b ,int sum)
-{
-printf("sum of %d and %d is%d\n",a,b,sum);
+
+void output(int a, int b, int sum){
+    printf("The sum of %d and %d is %d",a,b,sum);
 }
-int main()
-{
-int a,b,sum;
-a=input();
-b=input();
-add(a,b,&sum);
+
+int main(){
+  int a,b,sum;
+  a=input();
+  b=input();
+  add(a,b,&sum);
+  output(a,b,sum);
+
 }
