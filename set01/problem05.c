83c356e202c68e195a07ef2e1bd4b7cfd5817ae4
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-01-03 16:56:07 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -10,37 +10,37 @@ int main(){
   c = input();
   largest = compare(a, b, c);
   output(a, b, c, largest);
-}
+  }
 int input(){
   int a;
   scanf("%d", &a);
   return a;
-}
+  }
 int compare(int a, int b, int c){
   if (a > b && a > c){
    return a;
-  }
+    }
   else if (b > a && b > c){
     return b;
-  }
+    }
   else if (c > a && c > b){
     return c;
-  }
+    }
   else {
     printf("invalid input");
+    }
   }
-}
 void output(int a, int b, int c, int largest){
   if (largest == a){
     printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
-  }
+    }
   else if(largest == b){
     printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
-  }
+    }
   else if (largest == c){
     printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
-  }
+    }
   else{
     printf("sorry, wrong input try again.\n");
+    }
   }
-}
