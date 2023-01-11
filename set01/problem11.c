ce742c1ec2ae04cd7fa856ec5551f1a0020f88ca
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-01-11 16:30:58 +0000

    solved

--- set01/problem11.c
+++ set01/problem11.c
@@ -2,32 +2,30 @@
 
 struct complex
 {
-    float real;
-    float imaginary;
+float real;
+float imaginary;
 };
 typedef struct complex Comp;
 
 Comp input_complex();
-Comp add_complex(Comp a, Comp b);
-void output(Comp a, Comp b, Comp sum);
+Comp add_complex(Comp a, Comp b, Comp sum);
 
 int main()
 {
-  Comp a,b, sum;
-   a=input_complex();
-   b=input_complex();
-   sum=add_complex(a,b);
-   output(a,b, sum);
-   return 0;
-}
-
+  Comp a, b, sum;
+  a=input_complex();
+  b=input_complex();
+  sum=add_complex(a,b);
+  output(a,b, sum);
+  return 0;
+  }
 Comp input_complex()
 {
   Comp n;
-  printf("Enter the real part and the imaginary part: \n");
+  printf("Enter the real part and imaginary part: \n");
   scanf("%f %f", &n.real, &n.imaginary);
   return n;
-}
+  }
 Comp add_complex(Comp a, Comp b)
 {
   Comp add;
@@ -35,8 +33,7 @@ Comp add_complex(Comp a, Comp b)
   add.imaginary = a.imaginary+b.imaginary;
   return add;
   }
-
 void output(Comp a, Comp b, Comp sum)
 {
-    printf("The sum of %f+%fi and %f+%fi is %f+%fi\n", a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary);
-}
+  printf("The sum of %f+%fi and %f+%fi is %f+%fi\n", a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary);
+  }
