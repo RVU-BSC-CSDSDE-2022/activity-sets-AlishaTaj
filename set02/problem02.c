6b2057eb70c1ff7ff9049d10e5afcbe3c439c13a
Alisha Taj <114483067+AlishaTaj@users.noreply.github.com>
2023-01-20 12:27:39 +0000

    solved

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 int input_side();
 int check_scalene(int a, int b, int c);
 void output(int a, int b, int c, int isscalene);
@@ -36,4 +36,3 @@ int main()
   output(a,b,c,isscalene);
   return 0;
   }
-  
